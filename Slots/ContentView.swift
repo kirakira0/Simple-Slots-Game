3db88aed2f8ee3af446f882e670f17172367a0b3
kirakira0 <kiratoal0@gmail.com>
2020-09-27 21:51:36 -0700

    Initial Commit

---
+++ Slots/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slots
+//
+//  Created by Kira Toal on 9/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
