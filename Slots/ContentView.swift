f1da06c9c3f79c5e0c8ade4430303c5eb16278e5
kirakira0 <kiratoal0@gmail.com>
2020-09-27 22:10:50 -0700

    Build graphical background w/ ZStack and Rectangles

--- Slots/ContentView.swift
+++ Slots/ContentView.swift
@@ -7,15 +7,29 @@
 
 import SwiftUI
 
+var primaryColor = Color(red: 38/255, green: 190/255, blue: 255/255)
+var secondaryColor = Color(red: 38/255, green: 255/255, blue: 186/255)
+
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // ZStack: a view that overlays its children, aligning them in both axes.
+        ZStack {
+            // Background
+            Rectangle()
+                .foregroundColor(primaryColor)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Rectangle()
+                .foregroundColor(secondaryColor)
+                .rotationEffect(Angle(degrees: 45))
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 11 Pro")
     }
 }
