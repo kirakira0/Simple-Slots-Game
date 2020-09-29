92f16c9a97f349ee8052de876eae3cf5a98e6285
kirakira0 <kiratoal0@gmail.com>
2020-09-29 14:18:59 -0700

    Clean up code, add comments

--- Slots/ContentView.swift
+++ Slots/ContentView.swift
@@ -1,17 +1,23 @@
 //
 //  ContentView.swift
-//  Slots
-//
+//  A simple slots game.
 //  Created by Kira Toal on 9/27/20.
+//
+// This code was adapted from a tutorial by CodeWithChris on Youtube.
+// The original tutorial can be found here:
+// https://www.youtube.com/watch?v=51Z9Kun7tFo&ab_channel=CodeWithChris
 //
 
 import SwiftUI
 
 struct ContentView: View {
     
-    public var primaryColor = Color(red: 38/255, green: 190/255, blue: 255/255)
-    public var secondaryColor = Color(red: 38/255, green: 255/255, blue: 186/255)
+    public var primaryColor = Color(red: 147/255, green: 196/255, blue: 209/255)
     
+    public var secondaryColor = Color(red: 62/255, green: 136/255, blue: 165/255)
+    
+    public var buttonColor = Color(red: 45/255, green: 95/255, blue: 110/255)
+
     private var symbols = ["apple", "star", "cherry"]
     
     // Stats let us automatically update the UI
@@ -28,17 +34,12 @@ struct ContentView: View {
         return true
     }
     
-    
     var body: some View {
         // ZStacks overlay children and align them in both axes.
         ZStack {
             // Background
             Rectangle()
                 .foregroundColor(primaryColor)
-                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            Rectangle()
-                .foregroundColor(secondaryColor)
-                .rotationEffect(Angle(degrees: 45))
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             // VStacks arrange child views in a vertical line.
             VStack {
@@ -48,12 +49,12 @@ struct ContentView: View {
                 // HStacks arrange child views in a horizontal line.
                 HStack {
                     Image(systemName: "heart.fill")
-                        .foregroundColor(.white)
+                        .foregroundColor(secondaryColor)
                     Text("SwiftUI Slots")
                         .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                         .foregroundColor(.white)
                     Image(systemName: "heart.fill")
-                        .foregroundColor(.white)
+                        .foregroundColor(secondaryColor)
                 }.scaleEffect(2)
                 
                 Spacer()
@@ -97,7 +98,6 @@ struct ContentView: View {
                         self.numbers[i] = Int.random(in: 0...self.symbols.count - 1)
                     }
                     // Check earnings
-                    // Check if all elements are equal
                     if getCardEquivalence(numbers: self.numbers) {
                         self.credits += self.betAmount * 10
                     } else {
@@ -108,12 +108,10 @@ struct ContentView: View {
                         .foregroundColor(.white)
                         .padding(.all, 10)
                         .padding([.leading, .trailing], 30)
-                        .background(Color.pink)
+                        .background(buttonColor)
                         .cornerRadius(20)
                 }
-                
                 Spacer()
-                
             }
         }
     }
